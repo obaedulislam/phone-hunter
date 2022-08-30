9972e654131101bdec699c68b47ea7b5e66f3c35
obaedulislam <obaed.cse@gmail.com>
2022-08-31 00:59:39 +0600

    no phone found message based on search result

--- phone.js
+++ phone.js
@@ -1,1 +1,45 @@
-console.log('Hello')
+const loadPhones = async(searchText) => {
+    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data);
+}
+
+const displayPhones = phones => {
+    const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = '';
+    phones = phones.slice(0, 20);
+
+    //No Phone Found
+    const noMatchingPhone = document.getElementById('no-phone-found');
+    if(phones.length === 0){
+        noMatchingPhone.classList.remove('d-none');
+    }
+    else{
+        noMatchingPhone.classList.add('d-none');
+    }
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card h-100 shadow rounded-3 border-0">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `;
+        phoneContainer.appendChild(phoneDiv);
+    })
+
+} 
+
+document.getElementById('btn-search').addEventListener('click', function(){
+    const searchField = document.getElementById('search-phone');
+    const searchText = searchField.value;
+    loadPhones(searchText)
+});
+
+//loadPhones();
+
